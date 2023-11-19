ee1f37da1d4cdf8fa7860a50eb9058de290af64d
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2023-11-20 00:45:55 +0900

    Add dataloader files

---
+++ dataloader/dataloader.py
@@ -0,0 +1,29 @@
+from torch.utils.data import DataLoader
+
+__all__ = ["MaskPairImageDataLoader"]
+
+from .dataset import MaskPairImageDataset
+
+
+class MaskPairImageDataLoader(DataLoader):
+    def __init__(
+        self,
+        masked_image_dir: str,
+        identity_image_dir: str,
+        unmasked_image_dir: str,
+        batch_size: int,
+        shuffle: bool = True,
+        num_workers: int = 2,
+    ) -> None:
+        self.dataset = MaskPairImageDataset(
+            masked_image_dir, identity_image_dir, unmasked_image_dir, transforms=None
+        )
+        self.dataset_length = len(self.dataset)
+        self.batch_size = batch_size
+
+        super().__init__(
+            dataset=self.dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            num_workers=num_workers,
+        )
--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -1,15 +1,20 @@
 import os
 import random
-from typing import Dict
+from typing import Dict, Tuple, Union
 
 import cv2
 import torch
+import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 
 
 class MaskPairImageDataset(Dataset):
     def __init__(
-        self, masked_image_dir: str, identity_image_dir: str, unmasked_image_dir: str
+        self,
+        masked_image_dir: str,
+        identity_image_dir: str,
+        unmasked_image_dir: str,
+        transforms: Union[transforms.Compose, None],
     ) -> None:
         """Dataset for image pairs with masked and unmasked face.
 
@@ -37,10 +42,12 @@ class MaskPairImageDataset(Dataset):
         |--- ...
 
         Args:
-            masked_image_dir: Path to the masked images.
-            identity_image_dir: Path to the identity images.
-            unmasked_image_dir: Path to the unmasked images, should be paired
+            masked_image_dir (str): Path to the masked images.
+            identity_image_dir (str): Path to the identity images.
+            unmasked_image_dir (str): Path to the unmasked images, should be paired
                 with the masked_image_dir.
+            transforms (transforms.Compose): Transforms object for data augmentation.
+
 
         """
         super(MaskPairImageDataset, self).__init__()
@@ -48,6 +55,7 @@ class MaskPairImageDataset(Dataset):
         self.masked_image_dir = masked_image_dir
         self.identity_image_dir = identity_image_dir
         self.unmasked_image_dir = unmasked_image_dir
+        self.transforms = transforms
 
         self.masked_image_list = os.listdir(masked_image_dir)
         self.identity_image_list = {
@@ -56,8 +64,8 @@ class MaskPairImageDataset(Dataset):
         }
 
     def __getitem__(self, idx: int) -> Dict:
-        label = self.masked_image_dir[idx]
-        identity = label.split(".")[0].split("_")[0]
+        label = self.masked_image_list[idx]
+        identity = label.split(".")[0]
 
         masked_image_path = os.path.join(self.masked_image_dir, label)
         unmasked_image_path = os.path.join(self.unmasked_image_dir, label)
@@ -75,9 +83,15 @@ class MaskPairImageDataset(Dataset):
         unmasked_image = cv2.cvtColor(unmasked_image, cv2.COLOR_BGR2RGB)
         identity_image = cv2.cvtColor(identity_image, cv2.COLOR_BGR2RGB)
 
-        masked_image = torch.from_numpy(masked_image)
-        unmasked_image = torch.from_numpy(unmasked_image)
-        identity_image = torch.from_numpy(identity_image)
+        masked_image = torch.permute(torch.from_numpy(masked_image), (2, 0, 1))
+        unmasked_image = torch.permute(torch.from_numpy(unmasked_image), (2, 0, 1))
+        identity_image = torch.permute(torch.from_numpy(identity_image), (2, 0, 1))
+
+        if self.transforms is not None:
+            identity_image = self.transforms(identity_image)
+            masked_image, identity_image = self.paired_transform(
+                masked_image, identity_image
+            )
 
         return {
             "masked_image": identity_image,
@@ -87,3 +101,10 @@ class MaskPairImageDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self.masked_image_list)
+
+    def paired_transform(
+        self, img1: torch.Tensor, img2: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Transform for paired images"""
+        # TODO
+        return img1, img2
